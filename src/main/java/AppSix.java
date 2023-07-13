1497df60c88e5ae83823169aecbf3988391e6329
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2023-07-13 13:58:17 -0300

    feat:adicionado app six

---
+++ src/main/java/AppSix.java
@@ -0,0 +1,45 @@
+//this example counts the amount of every char present in a given string
+
+import java.util.Scanner;
+
+public class AppSix {
+
+    public static void main(String[] args) {
+        System.out.println("Program begins ==================");
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Type the word to analyse: ");
+        String userInputConverted = userInput.nextLine();
+        char[] charList = new char[userInputConverted.length()];
+
+        for(int c=0;c < userInputConverted.length();c++){
+            if(!isCharPresent(charList,userInputConverted.charAt(c)))  charList[c] = userInputConverted.charAt(c);
+        }
+
+        System.out.println("The char count is:");
+        for (char value : charList) {
+            int charCount = countChar(userInputConverted,value);
+            if(charCount > 0)  System.out.println(" " + value + ": " + charCount);
+        }
+        System.out.println();
+        System.out.println("Program ends   ==================");
+    }
+
+
+    public static int countChar(String originalString, char charToCount){
+        int totalCharCount = 0;
+
+        for(int c=0;c<originalString.length();c++){
+            if(originalString.charAt(c)==charToCount) totalCharCount++;
+        }
+        return totalCharCount;
+    }
+
+
+    public static boolean isCharPresent(char[] charArray, char charToFind){
+        for (char value : charArray) {
+            if (value == charToFind) return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/AppSixTest.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppSixTest {
+
+
+    AppSix appSix = new AppSix();
+
+    @Test
+    void shouldReturnCorrectCharCount() {
+        String originalValue = "This is some example";
+        char charToCount = 'e';
+        int expectedResult = 3;
+        int actualResult = appSix.countChar(originalValue,charToCount);
+
+        assertEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    void shouldReturnZeroForNoChar() {
+        String originalValue = "This is some example";
+        char charToCount = '0';
+        int expectedResult = 0;
+        int actualResult = appSix.countChar(originalValue,charToCount);
+
+        assertEquals(expectedResult,actualResult);
+    }
+
+
+    @Test
+    void shouldReturnTrueWhenFindChar(){
+        char[] charArray = new char[5];
+        charArray[0] = 'b';
+        charArray[1] = 'y';
+        charArray[2] = 'n';
+        charArray[3] = 'o';
+        charArray[4] = 'p';
+
+        boolean result = appSix.isCharPresent(charArray,'o');
+        assertTrue(result);
+    }
+
+
+    @Test
+    void shouldReturnFalseWhenDontFindChar(){
+        char[] charArray = new char[5];
+        charArray[0] = 'b';
+        charArray[1] = 'y';
+        charArray[2] = 'n';
+        charArray[3] = 'o';
+        charArray[4] = 'p';
+
+        boolean result = appSix.isCharPresent(charArray,'r');
+        assertFalse(result);
+    }
+}
