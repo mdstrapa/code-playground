3e7cb6016027a9eda54e3538e66f366634f9e12c
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2023-07-18 10:32:48 -0300

    refac: password generator improved

--- src/main/java/AppSevenPasswordGenerator.java
+++ src/main/java/AppSevenPasswordGenerator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class AppSevenPasswordGenerator {
@@ -5,41 +7,76 @@ public class AppSevenPasswordGenerator {
     static char[] simbols = {'!','@','#','$','%','&','*'};
     static char[] upperLetters = {'A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','Y','Z'};
     static char[] lowerLetters = {'a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','y','z'};
+    static char[] digits = {'0','1','2','3','4','5','6','7','8','9'};
 
     public static void main(String[] args) {
-        System.out.println("Password Generatos Begin --------------------------------------");
+        System.out.println("Password Generator Begin --------------------------------------");
 
         System.out.println(passwordGenerator());
 
-        System.out.println("Password Generatos End   --------------------------------------");
+        System.out.println("Password Generator End   --------------------------------------");
     }
 
     public static String passwordGenerator(){
+        List<PasswordRule> pendingRuleList = new ArrayList<>();
+        pendingRuleList.add(PasswordRule.SPECIAL_CHAR);
+        pendingRuleList.add(PasswordRule.UPPER_LETTER);
+        pendingRuleList.add(PasswordRule.LOWER_LETTER);
+        pendingRuleList.add(PasswordRule.DIGIT);
+
+        List<PasswordRule> doneRuleList = new ArrayList<>();
+
         Random random = new Random();
-        final int PASSOWORD_LENGHT = 8;
+        final int PASSOWORD_LENGHT = 12;
         int randomNumber;
+        int randomRule;
         StringBuilder passwordBuild = new StringBuilder();
 
         for(int c = 0;c<PASSOWORD_LENGHT;c++){
-            if(c==0 || c==4) {
-                randomNumber = random.nextInt(7);
-                passwordBuild.append(simbols[randomNumber]);
+
+            if(pendingRuleList.size()>0) {
+                randomRule = random.nextInt(pendingRuleList.size());
+                if(pendingRuleList.get(randomRule)==PasswordRule.SPECIAL_CHAR){
+                    randomNumber = random.nextInt(simbols.length);
+                    passwordBuild.append(simbols[randomNumber]);
+                } else if(pendingRuleList.get(randomRule)==PasswordRule.UPPER_LETTER) {
+                    randomNumber = random.nextInt(upperLetters.length);
+                    passwordBuild.append(upperLetters[randomNumber]);
+                }else if(pendingRuleList.get(randomRule)==PasswordRule.LOWER_LETTER) {
+                    randomNumber = random.nextInt(lowerLetters.length);
+                    passwordBuild.append(lowerLetters[randomNumber]);
+                } else {
+                    randomNumber = random.nextInt(digits.length);
+                    passwordBuild.append(digits[randomNumber]);
+                }
+
+                doneRuleList.add(pendingRuleList.get(randomRule));
+                pendingRuleList.remove(randomRule);
             }
-            if(c==1 || c==5) {
-                randomNumber = random.nextInt(23);
-                passwordBuild.append(upperLetters[randomNumber]);
+            else {
+                randomRule = random.nextInt(doneRuleList.size());
+                if(doneRuleList.get(randomRule)==PasswordRule.SPECIAL_CHAR){
+                    randomNumber = random.nextInt(simbols.length);
+                    passwordBuild.append(simbols[randomNumber]);
+                } else if(doneRuleList.get(randomRule)==PasswordRule.UPPER_LETTER) {
+                    randomNumber = random.nextInt(upperLetters.length);
+                    passwordBuild.append(upperLetters[randomNumber]);
+                }else if(doneRuleList.get(randomRule)==PasswordRule.LOWER_LETTER) {
+                    randomNumber = random.nextInt(lowerLetters.length);
+                    passwordBuild.append(lowerLetters[randomNumber]);
+                } else {
+                    randomNumber = random.nextInt(digits.length);
+                    passwordBuild.append(digits[randomNumber]);
+                }
             }
-            if(c==2 || c==6) {
-                randomNumber = random.nextInt(23);
-                passwordBuild.append(lowerLetters[randomNumber]);
-            }
-            if(c==3 || c==7) {
-                randomNumber = random.nextInt(9);
-                passwordBuild.append(randomNumber);
-            }
+
         }
 
         return passwordBuild.toString();
     }
 
+}
+
+enum PasswordRule{
+    SPECIAL_CHAR,UPPER_LETTER,LOWER_LETTER,DIGIT
 }
--- src/test/java/AppSevenPasswordGeneratorTest.java
+++ src/test/java/AppSevenPasswordGeneratorTest.java
@@ -1,27 +1,49 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppSevenPasswordGeneratorTest {
 
     @Test
     void shouldReturnCorrectPasswordSize() {
-        int expectedPasswordSize = 8;
-
+        int expectedPasswordSize = 12;
         String password = AppSevenPasswordGenerator.passwordGenerator();
-
         assertEquals(expectedPasswordSize,password.length());
     }
-
 
     @Test
-    void shouldHasSymbol() {
-        int expectedPasswordSize = 8;
-
+    void shouldReturnTrueIfHasSpecialChar() {
+        Pattern specialChar = Pattern.compile ("[!@#$%&*]");
         String password = AppSevenPasswordGenerator.passwordGenerator();
-
-        assertEquals(expectedPasswordSize,password.length());
-
-        assertEquals(expectedPasswordSize,password.length());
+        Matcher hasSpecialChar = specialChar.matcher(password);
+        assertTrue(hasSpecialChar.find());
     }
+
+    @Test
+    void shouldReturnTrueIfHasUpperLetter() {
+        Pattern specialChar = Pattern.compile ("[A-Z]");
+        String password = AppSevenPasswordGenerator.passwordGenerator();
+        Matcher hasSpecialChar = specialChar.matcher(password);
+        assertTrue(hasSpecialChar.find());
+    }
+
+    @Test
+    void shouldReturnTrueIfHasLowerLetter() {
+        Pattern specialChar = Pattern.compile ("[a-z]");
+        String password = AppSevenPasswordGenerator.passwordGenerator();
+        Matcher hasSpecialChar = specialChar.matcher(password);
+        assertTrue(hasSpecialChar.find());
+    }
+
+    @Test
+    void shouldReturnTrueIfHasDigit() {
+        Pattern specialChar = Pattern.compile ("[0-9]");
+        String password = AppSevenPasswordGenerator.passwordGenerator();
+        Matcher hasSpecialChar = specialChar.matcher(password);
+        assertTrue(hasSpecialChar.find());
+    }
+
 }
