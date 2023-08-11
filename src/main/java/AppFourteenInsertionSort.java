6b190f41c01a496b0d5d0bd3cda3450d462e34c3
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2023-08-11 10:45:20 -0300

    feat: Insertion Sort second commit

--- src/main/java/AppFourteenInsertionSort.java
+++ src/main/java/AppFourteenInsertionSort.java
@@ -13,48 +13,42 @@ public class AppFourteenInsertionSort {
         List<Integer> sortedList = new ArrayList<>();
         boolean positionFound;
         int indexSortedList;
-        int indexUnsortedList = 0;
 
         while (!unsortedList.isEmpty()){
 
             //get the element to sort
-            Integer element = unsortedList.get(indexUnsortedList);
+            Integer element = unsortedList.get(0);
             positionFound = false;
             indexSortedList = 0;
 
             //find the correct position in the sorted list
             while (!positionFound){
 
-                if(sortedList.isEmpty()) positionFound = true;
+                if(sortedList.size() == indexSortedList) positionFound = true;
+                else{
+                    if(sortedList.get(indexSortedList) > element) positionFound = true;
+                    else indexSortedList++;
+                }
+            }
 
-                else if(sortedList.size() == 1){
-                    if(sortedList.get(0) > element) positionFound = true;
-                    else if(sortedList.size() == indexSortedList) positionFound = true;
+            //if sorted list is empty all we have to do is to add the very first element to the sorted list
+            if(sortedList.isEmpty()) sortedList.add(element);
+            else {
+                //we need to increase the position of all bigger elements in oder to have a 'free position' to place the new sorted element
+                for(int i = sortedList.size() - 1;i >= indexSortedList;i--){
+                    if(i == sortedList.size() - 1) sortedList.add(sortedList.get(i));
+                    else sortedList.set(i + 1,sortedList.get(i));
                 }
 
-                else if(sortedList.get(indexSortedList) < element && sortedList.get(indexSortedList + 1) > element) positionFound = true;
-
-                indexSortedList++;
+                //insert the element in the position
+                //if the elemte is the last we need to add insted set
+                sortedList.set(indexSortedList, element);
             }
-
-            //we need to increase the position of all bigger elements in oder to have a 'free position' to place the new sorted element
-            for(int i = sortedList.size() - 1;i > indexSortedList;i--){
-                if(i == sortedList.size() - 1) sortedList.add(sortedList.get(i));
-                else sortedList.set(i + 1,sortedList.get(i));
-            }
-
-            //insert the element in the position
-            //if the elemte is the last we need to add insted set
-            sortedList.set(indexSortedList, element);
 
             //remove the elemente from the original list
-            unsortedList.remove(indexUnsortedList);
-
-            //goes to the next unsortedElement
-            indexUnsortedList++;
+            unsortedList.remove(0);
 
         }
-
 
         return sortedList;
     }
--- src/test/java/AppFourteenInsertionSortTest.java
+++ src/test/java/AppFourteenInsertionSortTest.java
@@ -16,19 +16,32 @@ class AppFourteenInsertionSortTest {
         unsortedList.add(1);
         unsortedList.add(7);
         unsortedList.add(3);
+        unsortedList.add(9);
+        unsortedList.add(11);
+        unsortedList.add(2);
+        unsortedList.add(20);
+        unsortedList.add(6);
 
         List<Integer> expectedResult = new ArrayList<>();
         expectedResult.add(1);
+        expectedResult.add(2);
         expectedResult.add(3);
         expectedResult.add(5);
+        expectedResult.add(6);
         expectedResult.add(7);
         expectedResult.add(8);
+        expectedResult.add(9);
+        expectedResult.add(11);
+        expectedResult.add(20);
 
         List<Integer> actualResult = AppFourteenInsertionSort.insertionSort(unsortedList);
 
         assertEquals(expectedResult.get(0),actualResult.get(0));
         assertEquals(expectedResult.get(2),actualResult.get(2));
         assertEquals(expectedResult.get(4),actualResult.get(4));
+        assertEquals(expectedResult.get(6),actualResult.get(6));
+        assertEquals(expectedResult.get(8),actualResult.get(8));
+        assertEquals(expectedResult.get(9),actualResult.get(9));
 
     }
 }
