e0187c6e81beee3248165fbbf027a91a911d3e28
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2023-09-01 17:02:24 -0300

    feat: simple improvment to bubble sort exibition

--- src/main/java/AppTwelveBubbleSort.java
+++ src/main/java/AppTwelveBubbleSort.java
@@ -11,12 +11,17 @@ public class AppTwelveBubbleSort {
         myList.add(1);
         myList.add(6);
         myList.add(7);
+        myList.add(15);
+        myList.add(11);
+        myList.add(3);
+        myList.add(8);
 
         bubbleSort(myList);
 
         System.out.println();
         System.out.println("The final list is:");
         showList(myList);
+        System.out.println();
         System.out.println();
 
         System.out.println("Program end   ==============================");
@@ -40,9 +45,16 @@ public class AppTwelveBubbleSort {
     }
 
     private static void showList(List<Integer> integerList){
+        integerList.forEach(AppTwelveBubbleSort::writeListNumber);
         System.out.println();
-        integerList.forEach(i -> System.out.print(i + " | "));
+    }
+
+    private static void writeListNumber(Integer number){
         System.out.println();
+        System.out.print(number + " - ");
+        for(int c = 0;c<number;c++){
+            System.out.print("*");
+        }
     }
 
 }
